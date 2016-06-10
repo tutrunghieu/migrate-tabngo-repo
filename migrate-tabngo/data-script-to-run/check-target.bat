44068ac45a102b50e779130d956a760b41607d99
henrytu <tutrunghieu@gmail.com>
2016-06-10 15:24:00 +0700

    Add script

---
+++ migrate-tabngo/data-script-to-run/check-target.bat
@@ -0,0 +1,14 @@
+cd C:\Users\Administrator\Desktop\migrate-tabngo
+
+"C:\Program Files\MongoDB\Server\3.2\bin\mongorestore"   -d  data-trenzi105   ./data-trenzi105
+
+rem java -jar migrate-tabngo.jar war-sum  -show true   -war ./migrate-tabngo.jar    -out  ./out-check1.txt 
+java -jar migrate-tabngo.jar war-sumtar -show true   -war ./migrate-tabngo.jar   -cmp ./out-check1.txt     -out  ./out-done1.txt 
+
+rem java -jar migrate-tabngo.jar folder-sum -show true   -in c:/opt/apps   -out  ./out-check2.txt 
+java -jar migrate-tabngo.jar folder-sumtar -show true   -in c:/opt/apps   -cmp ./out-check2.txt    -out  ./out-done2.txt 
+
+rem java -jar migrate-tabngo.jar mongo-sum -show true  -db data-trenzi105   -out ./out-check3.txt
+java -jar migrate-tabngo.jar mongo-sumtar -show true  -db data-trenzi105   -cmp ./out-check3.txt   -out ./out-done3.txt
+	
+pause
