4caf095b0009f1d730ea1f763caecb4577bef820
henrytu <tutrunghieu@gmail.com>
2016-06-10 15:04:59 +0700

    Cleaning

--- migrate-tabngo/src-jun08-database-listing/apps/tabngo/cmd/models/WarChecksum.java
+++ migrate-tabngo/src-jun08-database-listing/apps/tabngo/cmd/models/WarChecksum.java
@@ -1,9 +1,5 @@
 package apps.tabngo.cmd.models;
 
-import java.io.PrintWriter;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 public class WarChecksum {
 
 	public String fileName = "";
